b768afa923e04198cf0a3f4fa832a6d99099c538
Kashi-S-A <Kashappaappannavar@gmail.com>
2026-02-17 16:42:18 +0530

    saved

---
+++ 20-SB-Project/src/main/java/com/tyss/first/Application.java
@@ -0,0 +1,14 @@
+package com.tyss.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext app = SpringApplication.run(Application.class, args);
+	}
+
+}
