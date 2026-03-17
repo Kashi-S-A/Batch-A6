fcda797473cb4573479dd38afbf1170e9f37ab32
Kashi-S-A <Kashappaappannavar@gmail.com>
2026-03-17 16:40:13 +0530

    JWT

--- 24-Spring-Security-Data-JPA/src/main/java/com/tyss/config/SecurityConfig.java
+++ 24-Spring-Security-Data-JPA/src/main/java/com/tyss/config/SecurityConfig.java
@@ -1,17 +1,32 @@
 package com.tyss.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.tyss.filter.AppFilter;
+import com.tyss.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private AppFilter appFilter;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) {
@@ -22,12 +37,36 @@ public class SecurityConfig {
 					.permitAll()
 					.anyRequest()
 					.authenticated())
-			.formLogin(c -> Customizer.withDefaults())
-			.logout(c -> Customizer.withDefaults());
+					.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+					.authenticationProvider(authenticationProvider())
+					.addFilterBefore(appFilter, UsernamePasswordAuthenticationFilter.class);
+		
+					//session creation policy stateless
+					// auth provider
+					// filter before(appfilter,uandpassauth)
 		
 		
 		return http.build();
 	}
+	
+	
+	// Auth manager
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+		
+		return config.getAuthenticationManager();
+	}
+	
+	// Auth Provider
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userService);
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
+	}
+	
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
--- 24-Spring-Security-Data-JPA/src/main/java/com/tyss/controller/UserController.java
+++ 24-Spring-Security-Data-JPA/src/main/java/com/tyss/controller/UserController.java
@@ -3,14 +3,18 @@ package com.tyss.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tyss.dto.LoginDTO;
 import com.tyss.entity.User;
 import com.tyss.repo.UserRepo;
+import com.tyss.service.JwtService;
 
 @RestController
 public class UserController {
@@ -21,19 +25,41 @@ public class UserController {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
+	@Autowired
+	private JwtService jwtService;
+
 	@PostMapping("/register")
-	public String register(@RequestBody User user) {
+	public ResponseEntity<String> register(@RequestBody User user) {
 		Optional<User> opt = userRepo.findByEmail(user.getEmail());
 
 		if (opt.isPresent()) {
-			return "Already Registered";
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Already Registered");
 		}
 
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
 
 		userRepo.save(user);
 
-		return "Registered successfully";
+		return ResponseEntity.status(HttpStatus.CREATED).body("Registered Successfully");
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
+
+		User user = userRepo.findByEmail(loginDTO.getEmail()).orElseThrow(() -> new RuntimeException("User Not Found"));
+
+		try {
+			if (passwordEncoder.matches(loginDTO.getPassword(), user.getPassword())) {
+
+				String token = jwtService.generateToken(user.getEmail());
+
+				return ResponseEntity.ok("Token : " + token);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ResponseEntity.badRequest().body("Invalid credentails");
 	}
 
 	@GetMapping("/home")
